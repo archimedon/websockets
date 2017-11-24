128295340ec2848d4ebc9e39b353b2eaaa4900a5
archimedon <ronniedz@icloud.com>
2017-11-24 08:56:55 -0800

    deploy test 5

--- src/main/scala/io/scalac/akka/http/websockets/Server.scala
+++ src/main/scala/io/scalac/akka/http/websockets/Server.scala
@@ -5,25 +5,31 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives
 import akka.stream.ActorMaterializer
 
-import scala.io.StdIn
-
-
+//import scala.io.StdIn
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
 import akka.stream.scaladsl.Flow
+
+//import akka.http.scaladsl.server.Directives._
+//import akka.http.scaladsl.server.Route
+
+
+import akka.actor.{Props, ActorSystem}
+//import akka.io.IO
+//import org.apache.commons.daemon._
 
 object Server extends App {
 
+  private[this] var started: Boolean = true
 
-  implicit val actorSystem = ActorSystem("akka-system")
+  val applicationName = "test-sockets"
+  implicit val actorSystem = ActorSystem(applicationName)
   implicit val flowMaterializer = ActorMaterializer()
 
   val config = actorSystem.settings.config
   val interface = config.getString("app.interface")
 
   val port = sys.env("PORT").toInt
-//  val port = config.getInt("app.port")
+  //  val port = config.getInt("app.port")
 
   import Directives._
 
@@ -38,20 +44,43 @@ object Server extends App {
     }
   }
 
-
   val echoService: Flow[Message, Message, _] = Flow[Message].map {
     case TextMessage.Strict(txt) => TextMessage("ECHO: " + txt)
     case _ => TextMessage("Message type unsupported")
   }
+
   val binding = Http().bindAndHandle(route, interface, port)
 
   //  println(s"Server is now online at http://$interface:$port\nPress RETURN to stop...")
   println(s"Server is now online at http://$interface:$port")
-//  StdIn.readLine()
+  //  StdIn.readLine()
 
-  import actorSystem.dispatcher
+  val mainThread = Thread.currentThread();
 
-  binding.flatMap(_.unbind()).onComplete(_ => actorSystem.shutdown())
-  println("Server is down...")
+  Runtime.getRuntime.addShutdownHook(new Thread() {
+    override def run = {
+
+      if (started) {
+        println(s"Shutting down: $applicationName Service")
+        started = false
+//        mainThread.join()
+        cleanUp();
+        Thread.currentThread.interrupt()
+      }
+    }})
+
+
+  def cleanUp() {
+
+    import actorSystem.dispatcher
+
+    println(s"Releasing bindings")
+    binding.flatMap(_.unbind()).onComplete(_ => actorSystem.shutdown())
+    Http().shutdownAllConnectionPools() ; //andThen { case _ => actorSystem.shutdown() }
+//    actorSystem.shutdown()
+    complete("Shutting down app")
+
+  }
+
 
 }
