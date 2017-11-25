55e9a5b63fff3eb81152334e19180157245d875f
archimedon <ronniedz@icloud.com>
2017-11-25 03:14:37 -0800

    deploy test 12

--- src/main/scala/io/scalac/akka/http/websockets/Server.scala
+++ src/main/scala/io/scalac/akka/http/websockets/Server.scala
@@ -65,7 +65,7 @@ object Server extends App {
   def exitListen() {
 
     StdIn.readLine match {
-      case x if isExit(x) => shutdown = true; println("Shutting down") ; cleanUp()
+      case x if isExit(x) => shutdown = false; println("Shutting down") ; cleanUp()
       case _              => println("No way out"); exitListen
     }
 
