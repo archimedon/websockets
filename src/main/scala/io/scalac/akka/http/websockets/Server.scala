33a0f555ab43c01cc36c140a73602f4de95ccb0a
archimedon <ronniedz@icloud.com>
2017-11-24 20:19:41 -0800

    deploy test 8

--- src/main/scala/io/scalac/akka/http/websockets/Server.scala
+++ src/main/scala/io/scalac/akka/http/websockets/Server.scala
@@ -28,8 +28,9 @@ object Server extends App {
   val config = actorSystem.settings.config
   val interface = config.getString("app.interface")
 
-  //  val port = sys.env("PORT").toInt
-  val port = 8080;
+  val port = sys.env("PORT").toInt
+  
+//  val port = 8080;
 //    if (sys.env.contains("PORT")) sys.env("PORT").toInt
 //    else config.getInt("app.port")
 
