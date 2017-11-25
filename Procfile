5693925e4ebd0f94a6497aeb57208d58e6ca3bf1
archimedon <ronniedz@icloud.com>
2017-11-24 20:00:01 -0800

    deploy test 6

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: target/universal/stage/bin/websocket-akka-http
+web: target/universal/stage/bin/websocket-akka-http -Dhttp.port=${PORT}
+console: target/universal/stage/bin/websocket-akka-http -main scala.tools.nsc.MainGenericRunner -usejavacp
---
+++ target/scala-2.11/api/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"io.scalac.akka" : [], "io.scalac.akka.http" : [], "io.scalac" : [], "io.scalac.akka.http.websockets" : [{"object" : "io\/scalac\/akka\/http\/websockets\/Server$.html", "name" : "io.scalac.akka.http.websockets.Server"}], "io" : []};
